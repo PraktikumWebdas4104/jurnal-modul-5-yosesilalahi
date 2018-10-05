992f86dd50678a80ced0612cab49cfd734a936ce
yosesilalahi <43519393+yosesilalahi@users.noreply.github.com>
2018-10-05 10:09:11 +0700

    registrasi
    form registrasi yose silalahi
    6701174007

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,34 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
-
-
-
-</body>
-</html>
+<form method="POST">
+	<table>
+		<tr>
+			<td>NIM</td>
+			<td>:</td>
+			<td><input type="text" name="nim"></td>
+		</tr>
+		<tr>
+			<td>NAMA</td>
+			<td>:</td>
+			<td><input type="text" name="nama"></td>
+		</tr>
+		<tr>
+			<td>E-mail</td>
+			<td>:</td>
+			<td><input type="text" name="email"></td>
+		</tr>
+		<tr>
+			<td>USERNAME</td>
+			<td>:</td>
+			<td><input type="text" name="usname"></td>
+		</tr>
+		<tr>
+			<td>PASSWORD</td>
+			<td>:</td>
+			<td><input type="password" name="pass"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td><input type="submit" name="submitReg" value="DAFTAR"></td>
+		</tr>
+	</table>
+</form>
