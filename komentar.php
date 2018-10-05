c6e93e0f047fcbd1c4effd2d3e01d37f9c3f1503
yosesilalahi <43519393+yosesilalahi@users.noreply.github.com>
2018-10-05 10:23:37 +0700

    komentar php
    bagian komentar php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,27 @@
-<?php
+<form method="POST">
+	komentar : <input type="text" name="komentar"> &nbsp;
+	<input type="submit" name="submit" value="cek">
 
-?>
+
+
+</form>
+
+<?php 
+	if (isset($_POST['submit'])) {
+		$komentar=$_POST['komentar'];
+
+
+		if (str_word_count($komentar)>=5) {
+			echo $komentar;
+			# code...
+		}elseif (str_word_count($komentar)<5) {
+			echo "error, kata kurang dari 5 kata";
+			# code...
+			# code...
+		}
+		# code...
+	}
+
+
+
+ ?>
